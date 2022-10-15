3d41ef4601ae973e535cff212117a05e6c9b18d5
erazer2 <98871895+erazer2@users.noreply.github.com>
2022-10-15 17:55:14 +0100

    Add files via upload
    cursor changed to hover on changeables
    company name and details changed to populate from javascript file so can change these easily

--- website/details.js
+++ website/details.js
@@ -1,17 +1,28 @@
 
+//change these variables  to make it easier for your company
 var invoiceNumber=124;
 var createdDate="January 1,2022";
 var dueDate="February 1,2022";
 var vatRate=0.2;
 
+var initialLogo="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png";
+var CompanyName="Erazer Triplet2";
+var CompanyStreet="Accacia Avenue";
+var CompanycityAndPostcode="BananaMan";
+
 
 
 function populateInvoice(){
 
-var invoiceNumber=124;
-    document.getElementById("InvoiceNumber").innerHTML=invoiceNumber;
+    document.getElementById("invoiceLogo").src=initialLogo;
 
+    document.getElementById("companyName").value=CompanyName;
+    document.getElementById("companyStreet").value=CompanyStreet;
+    document.getElementById("companyCityAndPostCode").value=CompanycityAndPostcode;
 
+    document.getElementById("invoiceNumber").value=invoiceNumber;
+    document.getElementById("invoiceDate").value=createdDate;
+    document.getElementById("invoiceDueDate").value=dueDate;
 
 }
 
@@ -25,9 +36,9 @@ function calcTotals(){
         totalSofar=totalSofar+parseFloat(document.getElementById(itemNumber).value);
 
     }
-    document.getElementById("TotalExVat").value=totalSofar;
-    document.getElementById("Vat").value=totalSofar*vatRate;
-    document.getElementById("totalIncVat").value=totalSofar*vatRate+parseFloat(totalSofar);
+    document.getElementById("TotalExVat").value=(totalSofar).toFixed(2);
+    document.getElementById("Vat").value=(totalSofar*vatRate).toFixed(2);
+    document.getElementById("totalIncVat").value=(totalSofar*vatRate+parseFloat(totalSofar)).toFixed(2);
 
 }
 
