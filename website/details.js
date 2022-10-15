224dd4c3f3819f67059483fa3d8b85fedafb1ef5
erazer2 <98871895+erazer2@users.noreply.github.com>
2022-10-15 17:20:15 +0100

    Add files via upload
    css made external
    javascript to calc the totals dynamically
    vat added

---
+++ website/details.js
@@ -0,0 +1,32 @@
+
+var invoiceNumber=124;
+var createdDate="January 1,2022";
+var dueDate="February 1,2022";
+var vatRate=0.2;
+
+
+
+function populateInvoice(){
+
+var invoiceNumber=124;
+    document.getElementById("InvoiceNumber").innerHTML=invoiceNumber;
+
+
+
+}
+
+function calcTotals(){
+
+    var totalSofar=0;
+
+    for (var i=1;i<4;i++){
+        itemNumber="Item"+i;
+
+        totalSofar=totalSofar+parseFloat(document.getElementById(itemNumber).value);
+
+    }
+    document.getElementById("TotalExVat").value=totalSofar;
+    document.getElementById("Vat").value=totalSofar*vatRate;
+    document.getElementById("totalIncVat").value=totalSofar*vatRate+parseFloat(totalSofar);
+
+}
